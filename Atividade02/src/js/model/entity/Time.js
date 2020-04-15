695fb308f2c9e3f7d931b65c14b88bcb4db41b30
“Arthur <barrosarthur773@gmail.com>
2020-04-14 20:46:22 -0400

    versão 0.1

---
+++ Atividade02/src/js/model/entity/Time.js
@@ -0,0 +1,6 @@
+export default class Time {
+
+    constructor(nome_time, ano, nome_tecnico, nome_time_derrotafn) {
+        Object.assign(this, { nome_time, ano, nome_tecnico, nome_time_derrotafn});
+    }
+}
